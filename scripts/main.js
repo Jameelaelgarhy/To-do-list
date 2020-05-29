9cab9545a20b6a72b85e29f06f965eebbddbd231
Jameelaelgarhy <66128051+Jameelaelgarhy@users.noreply.github.com>
2020-05-29 15:37:04 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function deleteElement(boxName)
+{
+    document.getElementById(boxName).style.display='none';
+}
+
+function deleteAll()
+{
+    document.getElementById('box1').style.display='none';
+    document.getElementById('box2').style.display='none';
+    document.getElementById('box3').style.display='none';
+}
